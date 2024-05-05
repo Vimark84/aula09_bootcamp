05b23233b67927049a31fe772a3ec1180795ca92
Marcos Vinicius <vini.markinhos@gmail.com>
2024-05-04 23:37:03 -0300

    Finanlizando aula

---
+++ logging_exemplos_erros.py
@@ -0,0 +1,7 @@
+from loguru import logger
+
+logger.debug("Um aviso para o desenvolvedor(ou eu mesmo) no futuro")
+logger.info("Informação importante do processo")
+logger.warning("Um aviso de que algo vai parar no futuro")
+logger.error("aconteceu uma falha")
+logger.critical("Aconteceu uma falha que aborta a aplicação")
